62075b32d567e2917d1ceea53d2da961d6a3b16e
vinymd789k <142630779+vinymd789k@users.noreply.github.com>
2023-10-24 10:25:11 -0300

    Adicionando versão 2.0 automação bing

--- Automação pontos bing.py
+++ Automação pontos bing.py
@@ -26,7 +26,7 @@ def farm_de_pontos (qtd_pesquisas, pesquisas_realizadas, pontos_adquiridos):
     pa.write("bing")
     pa.press("enter")
 
-    time.sleep(5)
+    time.sleep(2)
 
     #farmando pontos
     for i in range(qtd_pesquisas):
@@ -44,7 +44,49 @@ def farm_de_pontos (qtd_pesquisas, pesquisas_realizadas, pontos_adquiridos):
 
         time.sleep(1.5)
 
+    pa.click(x = 1701, y = 178, button = "left", duration = 1)
+    
+    #define quantos tab a serem loopados
+    loop_tab = 7
+    #conta quantos tabs foram pressionados
+    contador_tab = 0
+    #lista com todas as posições de loop_tab a serem puladas.
+    pular_loop_tab = [10, 11, 12, 15]
+    #variável de continuidade loop while
+    while_infinite = 1
+
+    #loop de tab e enter
+    while while_infinite == 1:
+
+        #faz loop de tab para 
+        while contador_tab < loop_tab:
+        
+            pa.press("tab")
+            contador_tab += 1
+    
+        #ser os tabs foram pressionados o suficiente, faça a presquisa e retorne a seleção de tab.
+        if contador_tab == loop_tab and loop_tab not in pular_loop_tab:
+            
+            pa.press("enter")
+            time.sleep(1.5)
+            pa.click(x = 1701, y = 178, button = "left", duration = 1)
+
+            #zerar o contador e garante que haverá mais um tab para acessar o próximo artigo.
+            contador_tab = 0
+            loop_tab += 1 
+            
+            #se foram realizados tabs o suficiente para acessar todos os sites, pare o loop e gere o relatório
+            if loop_tab == 15:
+                break 
+
+        #reconhece se foram pressionados tabs o suficiente para chegar ao site que pode estragar o processo.
+        elif loop_tab in pular_loop_tab:
+            
+            pa.press("tab")
+            loop_tab += 1
+
     #relatório
-    return print(f"foram realizadas {pesquisas_realizadas} de pesquisas e farmados {pontos_adquiridos} de pontos")
+
+    return print(f"foram realizadas {pesquisas_realizadas} de pesquisas e farmados {pontos_adquiridos+100} de pontos")
 
 farm_de_pontos(qtd_pesquisas, pesquisas_realizadas, pontos_adquiridos)
